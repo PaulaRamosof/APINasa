6bbbf8f4d9d09a031c6065c18719c8e8f90133d0
Daniel Zamora Paez <[email>
2025-08-12 12:56:55 -0500

    Cambios js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 const key = 'RjGXDgj8bLifcEuErZtyc5vQE5aHyxxW74WWbynI';
+const url = 'https://api.nasa.gov/planetary/apod?api_key=' + key;
+const fecha = document.getElementById('consultarFecha');
+let x = JSON.parse(localStorage.getItem("")) || [];
+fetch(url, {
+    method: "GET",
+    body: JSON.stringify({
+        id: 1,
+        tittle: '',
+        date: "",
+        url: '',
+        explanation: "Miranda",
+        copyright: ''
+    }),
+    headers: {
+        "Content-Type": "application/json; charset=UTF-8"
+    }
+
+})
